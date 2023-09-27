b0f98d8c027eac73f3a0f432625728762d87fa62
Stefano Puzzuoli <stefano.puzzuoli2@gmail.com>
2023-09-27 22:49:54 +0100

    Add basic back-end authentication for signup

--- com.restaurant/src/main/java/com/restaurant/dao/UserDao.java
+++ com.restaurant/src/main/java/com/restaurant/dao/UserDao.java
@@ -2,6 +2,9 @@ package com.restaurant.dao;
 
 import com.restaurant.POJO.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface UserDao extends JpaRepository<User, Integer> {
+
+    User findByEmailId(@Param("email") String email);
 }
--- com.restaurant/src/main/java/com/restaurant/serviceImpl/UserServiceImpl.java
+++ com.restaurant/src/main/java/com/restaurant/serviceImpl/UserServiceImpl.java
@@ -1,32 +1,59 @@
 package com.restaurant.serviceImpl;
 
+import com.restaurant.POJO.User;
 import com.restaurant.constants.RestaurantConstants;
+import com.restaurant.dao.UserDao;
 import com.restaurant.service.UserService;
 import com.restaurant.utils.RestaurantUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
+import java.util.Objects;
 
 @Slf4j
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Autowired
+    UserDao userDao;
+
     @Override
     public ResponseEntity<String> signUp(Map<String, String> requestMap) {
         log.info("Inside signup {}", requestMap);
-        if (validateSignUpMap(requestMap)) {
-            
-        }
-        else {
-            return RestaurantUtils.getResponseEntity(RestaurantConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
+        try {
+            if (validateSignUpMap(requestMap)) {
+                User user = userDao.findByEmailId(requestMap.get("email"));
+                if (Objects.isNull(user)) {
+                    userDao.save(getUserFromMap(requestMap));
+                } else {
+                    return RestaurantUtils.getResponseEntity("Email already exists.", HttpStatus.BAD_REQUEST);
+                }
+            } else {
+                return RestaurantUtils.getResponseEntity(RestaurantConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
+            }
+            return RestaurantUtils.getResponseEntity("Successfully Registered.", HttpStatus.OK);
+        } catch (Exception ex) {
+            return RestaurantUtils.getResponseEntity(RestaurantConstants.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     private boolean validateSignUpMap(Map<String, String> requestMap) {
         return requestMap.containsKey("name") && requestMap.containsKey("contactNumber")
-                && requestMap.containsKey("email") && requestMap.containsKey("password"))
+                && requestMap.containsKey("email") && requestMap.containsKey("password");
+    }
+
+    private User getUserFromMap(Map<String, String> requestMap) {
+        User user = new User();
+        user.setName(requestMap.get("name"));
+        user.setContactNumber(requestMap.get("contactNumber"));
+        user.setEmail(requestMap.get("email"));
+        user.setPassword(requestMap.get("password"));
+        user.setRole(requestMap.get("role"));
+        user.setStatus(requestMap.get("status"));
+        return user;
     }
 }
