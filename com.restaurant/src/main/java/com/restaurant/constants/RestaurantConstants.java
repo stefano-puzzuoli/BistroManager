df7fc0bd552931c039e3f270bfaa92765b3cbf6c
Stefano Puzzuoli <stefano.puzzuoli2@gmail.com>
2023-09-27 22:25:39 +0100

    Add base sign up functionality

--- com.restaurant/src/main/java/com/restaurant/constants/RestaurantConstants.java
+++ com.restaurant/src/main/java/com/restaurant/constants/RestaurantConstants.java
@@ -3,4 +3,5 @@ package com.restaurant.constants;
 public class RestaurantConstants {
 
     public static final String SOMETHING_WENT_WRONG = "Something went Wrong.";
+    public static final String INVALID_DATA = "Invalid Data.";
 }
--- com.restaurant/src/main/java/com/restaurant/serviceImpl/UserServiceImpl.java
+++ com.restaurant/src/main/java/com/restaurant/serviceImpl/UserServiceImpl.java
@@ -1,8 +1,32 @@
 package com.restaurant.serviceImpl;
 
+import com.restaurant.constants.RestaurantConstants;
 import com.restaurant.service.UserService;
+import com.restaurant.utils.RestaurantUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
+@Slf4j
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+        log.info("Inside signup {}", requestMap);
+        if (validateSignUpMap(requestMap)) {
+            
+        }
+        else {
+            return RestaurantUtils.getResponseEntity(RestaurantConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private boolean validateSignUpMap(Map<String, String> requestMap) {
+        return requestMap.containsKey("name") && requestMap.containsKey("contactNumber")
+                && requestMap.containsKey("email") && requestMap.containsKey("password"))
+    }
 }
