43079561b1120bd04e94cb767a01cdc2e2a4550c
Stefano Puzzuoli <stefano.puzzuoli2@gmail.com>
2023-09-29 21:59:44 +0100

    Add JWT Authentication

--- com.restaurant/src/main/java/com/restaurant/POJO/User.java
+++ com.restaurant/src/main/java/com/restaurant/POJO/User.java
@@ -1,10 +1,12 @@
 package com.restaurant.POJO;
 
-import jakarta.persistence.*;
+
 import lombok.Data;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 
+
+import javax.persistence.*;
 import java.io.Serializable;
 
 @NamedQuery(name = "User.findByEmailId", query = "select u from User u where u.email=:email")
