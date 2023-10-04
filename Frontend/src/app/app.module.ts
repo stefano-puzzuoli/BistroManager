415bcf98a8a60b9a663b3d0aacf7db74663dd6f7
Stefano Puzzuoli <stefano.puzzuoli2@gmail.com>
2023-10-04 22:48:21 +0100

    Implement Signup form

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -15,6 +15,19 @@ import { AppHeaderComponent } from './layouts/full/header/header.component';
 import { AppSidebarComponent } from './layouts/full/sidebar/sidebar.component';
 import { HttpClientModule } from '@angular/common/http';
 import { SignupComponent } from './signup/signup.component';
+import { NgxUiLoaderConfig, SPINNER, NgxUiLoaderModule} from 'ngx-ui-loader';
+
+const ngxUiLoaderConfig: NgxUiLoaderConfig = {
+  text: "Loading...",
+  textColor:"#FFFFFF", 
+  textPosition:"center-center",
+  bgsColor:"#7b1fa2",
+  fgsColor:"#7b1fa2",
+  fgsType:SPINNER.squareJellyBox,
+  fgsSize:100,
+  hasProgressBar:false
+}
+
 
 @NgModule({
   declarations: [	
@@ -35,7 +48,8 @@ import { SignupComponent } from './signup/signup.component';
     MaterialModule,
     FlexLayoutModule,
     SharedModule,
-    HttpClientModule
+    HttpClientModule,
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
