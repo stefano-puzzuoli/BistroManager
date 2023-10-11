0499f90cd7ba7e101473c26d891c089e8b6fe115
Stefano Puzzuoli <stefano.puzzuoli2@gmail.com>
2023-10-11 16:09:12 +0100

    Add Change Password and Logout Functionalities

--- Frontend/src/app/services/user.service.ts
+++ Frontend/src/app/services/user.service.ts
@@ -33,4 +33,10 @@ export class UserService {
     return this.httpClient.get(this.url + "/user/checkToken");
   }
 
+  changePassword(data: any) {
+    return this.httpClient.post(this.url + "/user/changePassword", data, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json')
+    })
+  }
+
 }
