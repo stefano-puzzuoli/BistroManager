c1b00b77ec0aa9fd12b7a172889ce5de73dc0ca7
Stefano Puzzuoli <stefano.puzzuoli2@gmail.com>
2023-10-11 20:23:16 +0100

    Manage Products - Add Page with Filter bar

--- Frontend/src/app/shared/menu-items.ts
+++ Frontend/src/app/shared/menu-items.ts
@@ -11,11 +11,11 @@ export interface Menu{
 
 const MENUITEMS = [
     {state:'dashboard' , name:'Dashboard', type:'link', icon:'dashboard', role:''},
-    {state:'category' , name:'Manage Category', type:'link', icon:'category', role:''},
-    {state:'product' , name:'Manage Product', type:'link', icon:'inventory_2', role:'admin'},
-    {state:'order' , name:'Manage Order', type:'link', icon:'shopping_cart', role:''},
-    {state:'bill' , name:'View Bill', type:'link', icon:'backup_table', role:''},
-    {state:'user' , name:'Manage User', type:'link', icon:'people', role:'admin'}
+    {state:'category' , name:'Manage Categories', type:'link', icon:'category', role:'admin'},
+    {state:'product' , name:'Manage Products', type:'link', icon:'inventory_2', role:'admin'},
+    {state:'order' , name:'Manage Orders', type:'link', icon:'shopping_cart', role:''},
+    {state:'bill' , name:'View Bills', type:'link', icon:'backup_table', role:''},
+    {state:'user' , name:'Manage Users', type:'link', icon:'people', role:'admin'}
 
 ]
 @Injectable()
