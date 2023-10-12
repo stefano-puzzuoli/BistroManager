698cc504d28d19f83155dbc7f3310d26b9773f8f
Stefano Puzzuoli <stefano.puzzuoli2@gmail.com>
2023-10-12 21:48:02 +0100

    Manage Orders Page

--- Frontend/src/app/material-component/material.routing.ts
+++ Frontend/src/app/material-component/material.routing.ts
@@ -3,6 +3,7 @@ import { DashboardComponent } from '../dashboard/dashboard.component';
 import { RouteGuardService } from '../services/route-guard.service';
 import { ManageCategoryComponent } from './manage-category/manage-category.component';
 import { ManageProductComponent } from './manage-product/manage-product.component';
+import { ManageOrderComponent } from './manage-order/manage-order.component';
 
 
 export const MaterialRoutes: Routes = [
@@ -22,6 +23,16 @@ export const MaterialRoutes: Routes = [
         canActivate:[RouteGuardService],
         data:{
             expectedRole: ['admin']
+        }
+    },
+
+    
+    {
+        path:'order',
+        component:ManageOrderComponent,
+        canActivate:[RouteGuardService],
+        data:{
+            expectedRole: ['admin' , 'user']
         }
     },
 ];
--- Frontend/src/app/services/product.service.ts
+++ Frontend/src/app/services/product.service.ts
@@ -43,7 +43,7 @@ export class ProductService {
   }
 
   getById(id:any){
-    return this.httpClient.get(this.url + "/product/getProductById/"+id);
+    return this.httpClient.get(this.url + "/product/getById/"+id);
   }
   
 }
